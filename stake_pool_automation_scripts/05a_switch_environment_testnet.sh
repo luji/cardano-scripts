ce6be135e7e6761babb1181700fa927648b156a5
Etienne <etienne.sauve@gmail.com>
2021-09-16 13:42:28 -0400

    removed git pull sh

--- stake_pool_automation_scripts/05a_switch_environment_testnet.sh
+++ stake_pool_automation_scripts/05a_switch_environment_testnet.sh
@@ -2,6 +2,6 @@ export NODE_CONFIG=testnet>> $HOME/.bashrc
 echo export NODE_CONFIG=testnet>> $HOME/.bashrc
 source ~/.bashrc
 
-./${HOME}/cardano_scripts/stake_pool_automation_scripts/03_create_startup_scripts.sh
+$HOME/cardano_scripts/stake_pool_automation_scripts/03_create_startup_scripts.sh
 
-./${HOME}/cardano_scripts/stake_pool_automation_scripts/04_install_gLiveView_monitoring_tool.sh
+$HOME/cardano_scripts/stake_pool_automation_scripts/04_install_gLiveView_monitoring_tool.sh
