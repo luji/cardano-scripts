5e0b93aaec1645677074c5f9e470cd8daa2da0fc
Etienne <etienne.sauve@gmail.com>
2021-09-16 13:46:19 -0400

    ww

--- stake_pool_automation_scripts/05b_switch_environment_mainnet.sh
+++ stake_pool_automation_scripts/05b_switch_environment_mainnet.sh
@@ -2,6 +2,6 @@ export NODE_CONFIG=mainnet>> $HOME/.bashrc
 echo export NODE_CONFIG=mainnet>> $HOME/.bashrc
 source ~/.bashrc
 
-$HOME/cardano_scripts/stake_pool_automation_scripts/03_create_startup_scripts.sh
+$HOME/cardano-scripts/stake_pool_automation_scripts/03_create_startup_scripts.sh
 
-$HOME/cardano_scripts/stake_pool_automation_scripts/04_install_gLiveView_monitoring_tool.sh
+$HOME/cardano-scripts/stake_pool_automation_scripts/04_install_gLiveView_monitoring_tool.sh
